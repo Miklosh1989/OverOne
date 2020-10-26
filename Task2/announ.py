efdfe5c2d963fcdfd083c2a1be0611f21754cdb5
Fitz_Chivel <korchahin1989@gmail.com>
2020-10-26 16:22:46 +0300

    correct

--- Task2/announ.py
+++ Task2/announ.py
@@ -1,15 +1,16 @@
-from datetime import datetime
+from datetime import date
 
 class Announ:
-    def __init__(self, heading, description, author, views = 1, dates=datetime.now()):
+
+    def __init__(self, heading, description, author):
         self.__heading = heading
         self.__description = description
         self.__author = author
-        self.__dates = dates
-        self.__views = views
+        self.__views = 0
+        self.__dates = date.today()
 
     def __str__(self):
-        return f'Заголовок: {self.__heading}, Описание: {self.__description}, Автор: {self.__author}, Дата: {self.__dates}, Количество просмотров: {self.__views}'
+        return f'Заголовок: {self.__heading}, Описание: {self.__description}, Автор: {self.__author}, Количество просмотров: {self.__views}, Дата: {self.__dates}'
 
     def get_heading(self):
         return self.__heading
@@ -17,11 +18,7 @@ class Announ:
         return self.__description
     def get_author(self):
         return self.__author
-    def get_views(self):
-        self.__views = self.__views + 1
-        return self.__views
-    def get_dates(self):
-        return self.__dates
+
 
     def set_heading(self, heading):
         self.__heading = heading
@@ -29,11 +26,10 @@ class Announ:
     def set_description(self, description):
         self.__description = description
 
-
-
     def viewing_ads(self):
         self.__views = self.__views + 1
-        return self.__heading, self.__description, self.__author, self.__dates
+
+        return self.__heading, self.__description, self.__author, self.__views,self.__dates
 
 
 
--- Task2/main.py
+++ Task2/main.py
@@ -1,12 +1,14 @@
 from Task2.announ import Announ
 
 def main():
-        announ1 = Announ("Продаю авто", "В хорошом качестве, автопробег меньше 1000", "Андрей")
-        announ1.viewing_ads()
-        announ1.viewing_ads()
-        print(announ1)
 
-        announ1.set_heading("продаю жену")
-        print(announ1)
+        announ1 = Announ("Продаю авто", "В хорошом качестве, автопробег меньше 1000", "Андрей")
+
+        print(announ1.viewing_ads())
+        print(announ1.viewing_ads())
+        print(announ1.viewing_ads())
+        announ1.set_heading("Меняю жену на нормальную бабу")
+        print(announ1.viewing_ads())
+
 
 main()
